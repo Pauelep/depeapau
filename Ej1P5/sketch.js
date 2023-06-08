f8cefc8300e497b56ff858eabccadebebf6d7c30
Paulalopez03 <Paulalopez@alu.easda.es>
2023-06-08 22:58:45 +0200

    guyyi

--- Ej1P5/sketch.js
+++ Ej1P5/sketch.js
@@ -7,12 +7,11 @@ let sketch1 = function(p) {
   let palette1, palette2;
 
   p.setup = function() {
-    p.createCanvas(500, 500, p.WEBGL);
-    N = p.random([4, 6, 8]);
-    s = p.width / (N + 2 * margin);
-    margin *= s;
+    let canvas1 = p.createCanvas(p.select('#canvas1').width, p.select('#canvas1').height, p.WEBGL);
+    canvas1.parent('canvas1');
     p.noStroke();
     p.noLoop();
+    resizeSketch1();
   };
 
   p.draw = function() {
@@ -88,153 +87,102 @@ let sketch1 = function(p) {
       }
     }
   }
+
+  p.windowResized = function() {
+    resizeSketch1();
+  };
+
+  function resizeSketch1() {
+    let containerWidth = p.select('#canvas1').width;
+    let containerHeight = p.select('#canvas1').height;
+    p.resizeCanvas(containerWidth, containerHeight);
+    N = p.random([4, 6, 8]);
+    s = containerWidth / (N + 2 * margin);
+    margin *= s;
+  }
 };
+
 let sketch2 = function(p) {
-  const circs = []; // array of circs
-  const num = 2; // num of eyes
-  const clrs = ["red", "green", "blue"]; // add more colors for inner circs
-  const autoUpdateNumRays = false; // change if you want
+  let cols = ['#F2BBC9', '#4971A6', '#02732A', '#F2BB16', '#F27405'];
+
+  let count = 10;
+  let area = count * count;
+  let w, h;
+  let xa, ya;
+  let uoff, voff;
+
+  let off = 10;
+
+  let pos = [];
+  let sa = [];
+  let col = [];
+
+  let bg = '#FFF0DC';
 
   p.setup = function() {
-    p.createCanvas(p.windowWidth, p.windowHeight);
-    p.background(32);
-
-    p.strokeWeight(2);
-    p.stroke(127);
-
-    // Insert 2 "eyes"
-    for (let n = 0; n < num; n++) {
-      insertEye(((n + 1) * p.width) / (num + 1), p.height / 2, p.width / (num + 1), clrs, n % 2 !== 0);
-    }
-
+    let canvas2 = p.createCanvas(p.select('#canvas2').width, p.select('#canvas2').height);
+    canvas2.parent('canvas2');
+    p.noStroke();
     p.frameRate(30);
+    resizeSketch2();
   };
-
-  function insertEye(x, y, r, clrs, backwards) {
-    // Create structure circ for inner rays, will not be displayed later
-    circs.push(new Circ(x, y, r * 0.05, clrs.length)); // clrs.length anchor positions
-    if (backwards) circs[circs.length - 1].da = -circs[circs.length - 1].da;
-    const poss = circs[circs.length - 1].getPos();
-
-    // For every anchor position
-    for (let i = 0; i < poss.length; i++) {
-      const p1 = poss[i];
-      const p2 = poss[(i + 1) % poss.length];
-      const midPoint = { x: p.lerp(p1.x, p2.x, 0.5), y: p.lerp(p1.y, p2.y, 0.5) };
-      // Create rotating structure circ
-      circs.push(new Circ(midPoint.x, midPoint.y, r * 0.05, clrs.length));
-      if (backwards) circs[circs.length - 1].da = -circs[circs.length - 1].da;
-      circs[circs.length - 1].a = -p.TWO_PI / poss.length + (p.TWO_PI * i) / poss.length;
-      const cp = circs[circs.length - 1].getPos();
-      // Create circ to visualize with num of rays and given color
-      circs.push(new Circ(cp[0].x, cp[0].y, r * 0.4, 17, clrs[i]));
-      if (backwards) circs[circs.length - 1].da = -circs[circs.length - 1].da;
-    }
-  }
 
   p.draw = function() {
-    p.blendMode(p.BLEND);
-    p.background(32);
+    p.background(bg);
 
-    p.push();
-    p.blendMode(p.ADD);
-    const per_n = circs.length / num;
-    for (let n = 0; n < num; n++) {
-      for (let i = 1; i < per_n; i += 2) {
-        const sc = circs[n * per_n + i]; // structure circle
-        const poss = sc.getPos();
-        sc.update();
+    for (let n = 0; n < area; n++) {
+      let posx = pos[n].x + w / 2;
+      let posy = pos[n].y + h / 2;
 
-        const dc = circs[n * per_n + i + 1]; // display circ
-        dc.show();
-        dc.update(poss[0].x, poss[0].y);
-        if (autoUpdateNumRays) {
-          dc.setNumAnchors(20 + 10 * p.sin(p.TWO_PI * p.floor(p.fract(p.frameCount / 40) * 10) / 10));
-        } else {
-          if (p.mouseX && p.mouseY) dc.setNumAnchors(p.map(p.mouseX, 0, p.width, 3, 40), p.map(p.mouseY, 0, p.height, 200, 2));
-        }
-      }
+      xa = p.map(n, -area, area, 0.1, Math.PI * uoff);
+      ya = p.map(n, -area, area, 0.1, 2 * Math.PI * voff);
+
+      let xoff = p.sin(sa[n] + xa);
+      let yoff = p.abs(p.sin(sa[n] + ya));
+
+      p.fill(0);
+      p.text(n + 1, posx, posy);
+
+      p.fill('#DED1B6');
+      p.rect(posx + off, posy + off, w * xoff, h * yoff, 15);
+
+      p.fill(col[n]);
+      p.rect(posx, posy, w * xoff, h * yoff, 15);
     }
-    p.pop();
-    // noLoop();
+
+    xa += 0.1;
+    ya += 0.1;
+    uoff += 0.05;
+    voff -= 0.01;
   };
 
-  p.mousePressed = function() {
-    if (p.mouseButton === p.LEFT) {
-      p.loop();
-    }
+  p.windowResized = function() {
+    resizeSketch2();
   };
 
-  // Class Circ
-  // define position and size and num of anchor points
-  // get positions of anchors after update() and getPos()
-  class Circ {
-    constructor(x, y, r, n, clr = null) {
-      this.x = x;
-      this.y = y;
-      this.r = r;
-      this.n = n;
-      this.a = 0;
-      this.da = p.TWO_PI / n / 16; // level of detail rotation > 1
-      this.clr = clr || p.color(128);
+  function resizeSketch2() {
+    let containerWidth = p.select('#canvas2').width;
+    let containerHeight = p.select('#canvas2').height;
+    p.resizeCanvas(containerWidth, containerHeight);
+    pos = new Array(area);
+    sa = new Array(area);
+    col = new Array(area);
+    w = containerWidth / count;
+    h = containerHeight / count;
+    uoff = 1;
+    voff = 1;
+    for (let n = 0; n < area; n++) {
+      let i = n % count;
+      let j = Math.floor(n / count);
+      pos[n] = p.createVector(i * w, j * h);
+      sa[n] = p.map(i * j, 0, area, -Math.PI, Math.PI);
+      let s = p.sin(i + j);
+      let ii = Math.floor(p.map(s, -1, 1, 0, cols.length));
+      ii = p.constrain(ii, 0, cols.length - 1);
+      col[n] = p.color(cols[ii]);
     }
-
-    setNumAnchors(n, t = 16) {
-      n = p.floor(n);
-      this.n = n;
-      this.da = p.TWO_PI / n / t * Math.sign(this.da);
-    }
-
-    getPos(n = null) {
-      let poss = [];
-      if (!n) n = this.n;
-
-      for (let i = 0; i < n; i++) {
-        const ca = (i / n) * p.TWO_PI;
-        const x = p.cos(this.a + ca) * this.r + this.x;
-        const y = p.sin(this.a + ca) * this.r + this.y;
-        poss.push({ x, y });
-      }
-
-      return poss;
-    }
-
-    update(x = null, y = null) {
-      this.a += this.da;
-      if (!(x === null || y === null)) {
-        this.x = x;
-        this.y = y;
-      }
-    }
-
-    show() {
-      p.push();
-      p.noFill();
-      p.stroke(this.clr);
-      p.translate(this.x, this.y);
-      p.rotate(this.a);
-
-      // circle(0, 0, this.r * 2);
-      // strokeWeight(2);
-      // line(0, -this.r, 0, this.r);
-
-      p.noStroke();
-      p.fill(this.clr);
-      const poss = this.getPos(this.n * 2);
-      for (let i = 0; i < poss.length - 1; i += 2) {
-        p.beginShape();
-        p.vertex(0, 0);
-        p.vertex(poss[i].x - this.x, poss[i].y - this.y);
-        p.vertex(poss[i + 1].x - this.x, poss[i + 1].y - this.y);
-        p.endShape(p.CLOSE);
-      }
-
-      p.pop();
-    }
-  };
+  }
 };
-
-
 
 let p1 = new p5(sketch1, 'canvas1');
 let p2 = new p5(sketch2, 'canvas2');
