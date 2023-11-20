96dc55b6d6b6780b1bcc0bf2d7ed1c172d899a0b
Pauelep <depeapau@gmail.com>
2023-11-20 23:55:42 +0100

    yjyj

--- maintwo.js
+++ maintwo.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
   $(".w-webflow-badge").removeClass("w-webflow-badge").empty();
 });
+
+function cambiarColorDeFondo() {
+  // Colores específicos para el degradado
+  const color1 = "#4F90FD";  // Azul
+  const color2 = "#00D14F";  // Rojo
+  const color3 = "#FD990F";  // Rojo
+
+  // Aplicar el degradado al fondo del body
+  document.body.style.background = `linear-gradient(to right, ${color1}, ${color2},${color3})`;
+
+  // Aplicar el degradado al fondo de .barra
+  document.querySelector('.barra').style.background = `linear-gradient(to right, ${color1}, ${color2},${color3})`;
+
+  // Restaurar el fondo original después de un segundo
+  setTimeout(() => {
+    document.body.style.background = "";
+    document.querySelector('.barra').style.background = "#ffffff"; // Restaurar el color original de .barra
+  }, 1000);
+}
+
+// Cambiar el color de fondo cada 5 segundos
+setInterval(cambiarColorDeFondo, 10000);
