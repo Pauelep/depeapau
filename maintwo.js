48dac26606d517463c5f54ad206f45865533e2c2
Pauelep <depeapau@gmail.com>
2023-11-22 01:41:00 +0100

    yfffgv

--- maintwo.js
+++ maintwo.js
@@ -2,24 +2,28 @@ $(document).ready(function () {
   $(".w-webflow-badge").removeClass("w-webflow-badge").empty();
 });
 
+const elementos = ["body", ".nav_overlay", ".barra"];
+
 function cambiarColorDeFondo() {
   // Colores específicos para el degradado
-  const color1 = "#4F90FD";  // Azul
-  const color2 = "#00D14F";  // Rojo
-  const color3 = "#FD990F";  // Rojo
+  const color1 = "#4F90FD";  // Azul
+  const color2 = "#00D14F";  // Rojo
+  const color3 = "#FD990F";  // Rojo
 
-  // Aplicar el degradado al fondo del body
-  document.body.style.background = `linear-gradient(to right, ${color1}, ${color2},${color3})`;
-
-  // Aplicar el degradado al fondo de .barra
-  document.querySelector('.barra').style.background = `linear-gradient(to right, ${color1}, ${color2},${color3})`;
+  // Aplicar el degradado al fondo de cada elemento
+  for (let i = 0; i < elementos.length; i++) {
+    const elemento = document.querySelector(elementos[i]);
+    elemento.style.background = `linear-gradient(to right, ${color1}, ${color2},${color3})`;
+  }
 
   // Restaurar el fondo original después de un segundo
   setTimeout(() => {
-    document.body.style.background = "";
-    document.querySelector('.barra').style.background = "#ffffff"; // Restaurar el color original de .barra
+    for (let i = 0; i < elementos.length; i++) {
+      const elemento = document.querySelector(elementos[i]);
+      elemento.style.background = "";
+    }
   }, 2000);
 }
 
 // Cambiar el color de fondo cada 5 segundos
-setInterval(cambiarColorDeFondo, 60000);
+setInterval(cambiarColorDeFondo, 10000);
